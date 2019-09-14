eb080b292577e1bfd45d647d07c4068b77f4c2b6
ahmed.r <ahmed.r@rakuten.com>
2019-09-14 15:32:13 +0900

    initial commit

---
+++ build/precache-manifest.11969333f6a6a69a0782a920d9c783fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "302d2b75cd54e56f8287",
+    "url": "/static/js/main.302d2b75.chunk.js"
+  },
+  {
+    "revision": "ac7ee9b192d985d18916",
+    "url": "/static/js/1.ac7ee9b1.chunk.js"
+  },
+  {
+    "revision": "302d2b75cd54e56f8287",
+    "url": "/static/css/main.55de31ff.chunk.css"
+  },
+  {
+    "revision": "b9481bb5454f31d194796fd3adfc5548",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.1a64c5d8b7838651f298ca9872b1d8f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3edfa1696f82edeadac3",
+    "url": "/static/js/main.3edfa169.chunk.js"
+  },
+  {
+    "revision": "ac7ee9b192d985d18916",
+    "url": "/static/js/1.ac7ee9b1.chunk.js"
+  },
+  {
+    "revision": "3edfa1696f82edeadac3",
+    "url": "/static/css/main.55de31ff.chunk.css"
+  },
+  {
+    "revision": "0f06db835064a29c94c889d3f567e0ec",
+    "url": "/index.html"
+  }
+];
